d1dd5d61d64afb7e0abbae10d17178b8bc8d4317
Jay <jvelayud@gmail.com>
2021-08-18 22:00:41 -0400

    Adding the API

---
+++ src/util/handler.js
@@ -0,0 +1,20 @@
+export default function handler(lambda) {
+  return async function (event, context) {
+    let body, statusCode;
+
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+    };
+  };
+}
