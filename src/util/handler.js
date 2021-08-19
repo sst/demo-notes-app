cd178f3f87ea7c9164d6ad7640ba0e92ea89bd2a
Jay <jvelayud@gmail.com>
2021-08-18 22:55:46 -0400

    Enabling CORS

--- src/util/handler.js
+++ src/util/handler.js
@@ -16,6 +16,10 @@ export default function handler(lambda) {
     return {
       statusCode,
       body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
     };
   };
 }
