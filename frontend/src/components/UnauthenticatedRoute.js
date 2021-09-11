b676ee7e33b09e3b93a7d49fd4a52f56ba35fb07
Jay <jvelayud@gmail.com>
2021-09-10 23:09:16 -0400

    Fixing passing state for child components for routes

--- frontend/src/components/UnauthenticatedRoute.js
+++ frontend/src/components/UnauthenticatedRoute.js
@@ -1,32 +1,30 @@
-import React from "react";
+import React, { cloneElement } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useAppContext } from "../lib/contextLib";
 
 function querystring(name, url = window.location.href) {
-  name = name.replace(/[[]]/g, "\\$&");
-
-  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i");
+  const parsedName = name.replace(/[[]]/g, "\\$&");
+  const regex = new RegExp(`[?&]${parsedName}(=([^&#]*)|&|#|$)`, "i");
   const results = regex.exec(url);
 
-  if (!results) {
-    return null;
-  }
-  if (!results[2]) {
-    return "";
+  if (!results || !results[2]) {
+    return false;
   }
 
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-export default function UnauthenticatedRoute({ children, ...rest }) {
+export default function UnauthenticatedRoute(props) {
+  const { children, ...rest } = props;
   const { isAuthenticated } = useAppContext();
   const redirect = querystring("redirect");
+
   return (
     <Route {...rest}>
       {!isAuthenticated ? (
-        children
+        cloneElement(children, props)
       ) : (
-        <Redirect to={redirect === "" || redirect === null ? "/" : redirect} />
+        <Redirect to={redirect ? redirect : "/"} />
       )}
     </Route>
   );
