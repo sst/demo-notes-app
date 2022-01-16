34da5f8b85f5c00be463c2a2c4d20d13f37746a8
Jon Holman <github@jonholman.com>
2022-01-15 20:33:55 -0500

    Update to SST 0.59.1 and CDK 2.7.0

--- test/StorageStack.test.js
+++ test/StorageStack.test.js
@@ -1,4 +1,4 @@
-import { expect, haveResource } from "@aws-cdk/assert";
+import { expect, haveResource } from "aws-cdk-lib/assertions";
 import * as sst from "@serverless-stack/resources";
 import StorageStack from "../stacks/StorageStack";
 
