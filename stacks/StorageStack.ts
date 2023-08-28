23aa6009f474a915471972174c0aaf9eb228b28b
Jay V <air@live.ca>
2023-08-28 17:54:34 -0400

    Cors

--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -2,7 +2,17 @@ import { Bucket, StackContext, Table } from "sst/constructs";
 
 export function StorageStack({ stack }: StackContext) {
   // Create an S3 bucket
-  const bucket = new Bucket(stack, "Uploads");
+  const bucket = new Bucket(stack, "Uploads", {
+    cors: [
+      {
+        maxAge: "1 day",
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+      },
+    ],
+  });
+
   // Create the DynamoDB table
   const table = new Table(stack, "Notes", {
     fields: {
