423af79f491b4440e28fe7bf1b867c0b79ee6717
Jay V <air@live.ca>
2023-08-28 16:34:36 -0400

    init

---
+++ sst.config.ts
@@ -0,0 +1,14 @@
+import { SSTConfig } from "sst";
+import { API } from "./stacks/MyStack";
+
+export default {
+  config(_input) {
+    return {
+      name: "notes",
+      region: "us-east-1",
+    };
+  },
+  stacks(app) {
+    app.stack(API);
+  },
+} satisfies SSTConfig;
---
+++ stacks/StorageStack.ts
@@ -0,0 +1,16 @@
+import { StackContext, Table } from "sst/constructs";
+
+export function StorageStack({ stack }: StackContext) {
+  // Create the DynamoDB table
+  const table = new Table(stack, "Notes", {
+    fields: {
+      userId: "string",
+      noteId: "string",
+    },
+    primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+  });
+
+  return {
+    table,
+  };
+}
