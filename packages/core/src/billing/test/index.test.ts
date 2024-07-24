4cf5bbee3b062c34850aa2f1f1f8a45380bb81fb
Jay V <air@live.ca>
2024-07-24 16:04:49 -0400

    Adding unit tests

---
+++ packages/core/src/billing/test/index.test.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "vitest";
+import { Billing } from "../";
+
+test("Lowest tier", () => {
+  const storage = 10;
+
+  const cost = 4000;
+  const expectedCost = Billing.compute(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+test("Middle tier", () => {
+  const storage = 100;
+
+  const cost = 20000;
+  const expectedCost = Billing.compute(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+test("Highest tier", () => {
+  const storage = 101;
+
+  const cost = 10100;
+  const expectedCost = Billing.compute(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
