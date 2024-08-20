308c0312a0572096d4d0fbe2aac8fa6d4996b4e2
Jay V <air@live.ca>
2024-08-20 17:47:01 -0400

    Updating v3 in package.json

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -3,38 +3,38 @@
 import "sst"
 declare module "sst" {
   export interface Resource {
-    Api: {
-      type: "sst.aws.ApiGatewayV2"
-      url: string
+    "Api": {
+      "type": "sst.aws.ApiGatewayV2"
+      "url": string
     }
-    Frontend: {
-      type: "sst.aws.StaticSite"
-      url: string
+    "Frontend": {
+      "type": "sst.aws.StaticSite"
+      "url": string
     }
-    IdentityPool: {
-      id: string
-      type: "sst.aws.CognitoIdentityPool"
+    "IdentityPool": {
+      "id": string
+      "type": "sst.aws.CognitoIdentityPool"
     }
-    Notes: {
-      name: string
-      type: "sst.aws.Dynamo"
+    "Notes": {
+      "name": string
+      "type": "sst.aws.Dynamo"
     }
-    StripeSecretKey: {
-      type: "sst.sst.Secret"
-      value: string
+    "StripeSecretKey": {
+      "type": "sst.sst.Secret"
+      "value": string
     }
-    Uploads: {
-      name: string
-      type: "sst.aws.Bucket"
+    "Uploads": {
+      "name": string
+      "type": "sst.aws.Bucket"
     }
-    UserPool: {
-      id: string
-      type: "sst.aws.CognitoUserPool"
+    "UserPool": {
+      "id": string
+      "type": "sst.aws.CognitoUserPool"
     }
-    UserPoolClient: {
-      id: string
-      secret: string
-      type: "sst.aws.CognitoUserPoolClient"
+    "UserPoolClient": {
+      "id": string
+      "secret": string
+      "type": "sst.aws.CognitoUserPoolClient"
     }
   }
 }
